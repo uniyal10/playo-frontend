666044c499525e2f084ea878aba39a28c7c05607
sudhanshu uniyal <uniyalsudhanshu10@gmail.com>
2023-02-26 20:32:18 +0530

    final commit

--- src/components/HomeGuest.js
+++ src/components/HomeGuest.js
@@ -47,7 +47,7 @@ function HomeGuest(props) {
                 <input onChange={e => setPassword(e.target.value)} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
               </div>
               <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
-                Sign up for Sporto
+                Sign up for playo
               </button>
             </form>
           </div>
